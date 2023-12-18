401e28592301e9c2d404cfb3871d99236d3558fa
Felipe Hora <111293503+FelipeTorresHora@users.noreply.github.com>
2023-12-17 21:29:16 -0300

    func.py

--- func.py
+++ func.py
@@ -10,3 +10,23 @@ class LeitorAcoes:
         with open(f"{self.caminho_arquivo}{acao}.txt","r") as arquivo_cotacao:
             linhas = arquivo_cotacao.readlines()
             self.dados = [linha.replace("\n","").split(";") for linha in linhas]
+class GerrenciadorPlanilha:
+    def __init__(self):
+        self.workbook = Workbook()
+        self.planilha_ativa = None
+    
+    def add_planilha(self,titulo_planilha: str=""):
+        nova_planilha = self.workbook.create_sheet(titulo_planilha)
+        self.workbook.active = nova_planilha
+        self.planilha_ativa = nova_planilha
+
+        return nova_planilha
+    def add_linha(self,dados:list):
+        self.planilha_ativa.append(dados)
+
+    def atualiza_celula(self,celula:str, dado):
+        self.planilha_ativa[celula] = dado
+    
+    def mescla_celula(self,celula_inicio:str,celula_fim:str):
+        self.planilha_ativa.merge_cells(f"{celula_inicio}:{celula_fim}")
+        
